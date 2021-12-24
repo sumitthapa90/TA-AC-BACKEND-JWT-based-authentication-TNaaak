e9bf1ab14352fa8c69a0d126a7a65ec846fffe69
sumit <sumitthapa90@gmail.com>
2021-12-24 17:19:50 +0530

    block-BNaafs

---
+++ block-BNaafs/Token-With-Api/routes/index.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var router = express.Router();
+var auth = require("../middlewere/auth");
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.json({ msg: "welcome" });
+});
+
+router.get("/dashboard", auth.verifyToken, (req, res, next) => {
+  res.json({ msg: "Welcom to dashboard" });
+});
+
+module.exports = router;
