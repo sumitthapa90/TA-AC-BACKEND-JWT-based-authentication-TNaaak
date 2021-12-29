fa61a6ef93eda71f11256e8a45f93127291cbee9
sumit <sumitthapa90@gmail.com>
2021-12-29 23:07:07 +0530

    block-BNaaen again

--- block-BNaaen/Conduit-App/routes/profiles.js
+++ block-BNaaen/Conduit-App/routes/profiles.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var User = require("../models/User");
 
+
 //GET /api/profiles/:username
 
 router.get("/:username", async (req, res, next) => {
